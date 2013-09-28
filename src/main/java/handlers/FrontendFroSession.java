39e9c6d54d403e85f88284edf6546c5870c27ac2
toha <tohasvs@gmail.com>
2013-09-28 10:43:08 +0400

    request

--- src/main/java/handlers/FrontendFroSession.java
+++ src/main/java/handlers/FrontendFroSession.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,8 +24,23 @@ import java.util.concurrent.atomic.AtomicLong;
  * To change this template use File | Settings | File Templates.
  */
 public class FrontendFroSession extends HttpServlet {
-    public String login = "";
     private AtomicLong  userIdGenerator = new AtomicLong();
+    private Map<String, Long> dataUserId;
+    private Map<Long, Long> dataSessionMapUser;
+
+    private void createSession() {
+        dataUserId = new HashMap<String, Long>();
+        dataSessionMapUser = new HashMap<Long, Long>();
+        dataUserId.put("user1", new Long(1));
+        dataUserId.put("user2", new Long(2));
+        dataUserId.put("user3", new Long(3));
+        dataUserId.put( "user4", new Long(4));
+    }
+
+    public FrontendFroSession() {
+        super();
+        createSession();
+    }
 
     public static String getTime() {
         Date date = new Date();
@@ -34,6 +50,7 @@ public class FrontendFroSession extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String login = "";
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         Map<String, Object> pageVariables = new HashMap<String, Object>();
@@ -52,15 +69,17 @@ public class FrontendFroSession extends HttpServlet {
 
         if (request.getPathInfo().equals("/userid")) {
             HttpSession session = request.getSession();
-            Long userId = (Long) session.getAttribute("userId");
-            if (userId == null) {
-                userId = userIdGenerator.getAndIncrement();
-                session.setAttribute("userId", userId);
-            }
-            pageVariables.put("serverTime", getTime());
-            pageVariables.put("refreshPeriod", "1000");
-            pageVariables.put("userId", userId);
+            Long sessionId = (Long) session.getAttribute("sessionId");
 
+
+
+           if (sessionId != null) {
+
+               pageVariables.put("serverTime", getTime());
+               pageVariables.put("refreshPeriod", "1000");
+               pageVariables.put("userId", dataSessionMapUser.get(sessionId));
+               pageVariables.put("sessionId", sessionId);
+           }
             response.getWriter().println(PageGenerator.getPage("userId.tml", pageVariables));
             return;
         }
@@ -74,11 +93,43 @@ public class FrontendFroSession extends HttpServlet {
 
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response) throws ServletException, IOException {
-        login = request.getParameter("login");
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
         Map<String, Object> pageVariables = new HashMap<String, Object>();
-        pageVariables.put("lastLogin", login);
-        response.getWriter().println(PageGenerator.getPage("authform.tml", pageVariables));
+        if (request.getPathInfo().equals("/userid"))  {
+
+            String login = request.getParameter("login");
+            String password = request.getParameter("password");
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+            if (login != null) {
+                if (dataUserId.containsKey(login)) {
+                    if (login.equals(password)) {
+                        HttpSession session = request.getSession();
+                        Long userId = dataUserId.get(login);
+
+                        Long sessionId = (Long) session.getAttribute("sessionId");
+                        if (sessionId == null) {
+                            sessionId = userIdGenerator.getAndIncrement();
+                            dataSessionMapUser.put(sessionId, userId);
+                            session.setAttribute("sessionId", sessionId);
+                        }
+                        else {
+                         response.getWriter().println(PageGenerator.getPage("timer.tml", pageVariables));
+                            return;
+                        }
+
+
+                        pageVariables.put("serverTime", getTime());
+                        pageVariables.put("refreshPeriod", "1000");
+                        pageVariables.put("userId", dataUserId.get(login));
+                        pageVariables.put("sessionId", sessionId);
+
+                        response.getWriter().println(PageGenerator.getPage("userId.tml", pageVariables));
+                        return;
+                    }
+                }
+            }
+        }
+        response.getWriter().println(PageGenerator.getPage("timer.tml", pageVariables));
+        return;
     }
 }
